ab4bc613bbd7037b3d7bd372537729c82a876e31
Janne Raiskila <janne@raiskila.com>
2015-10-07 02:42:06 +0300

    rm unnecessary import

--- VectorMath/VectorMath+Types.swift
+++ VectorMath/VectorMath+Types.swift
@@ -31,8 +31,6 @@
 //  3. This notice may not be removed or altered from any source distribution.
 //
 
-import Foundation
-
 typealias Vector2    = __Vector2
 typealias Vector3    = __Vector3
 typealias Vector4    = __Vector4
